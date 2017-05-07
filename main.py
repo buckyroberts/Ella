8e9dd5a98f26180b1d7f7b771184345279e09dd1
Bucky <buckyroberts@gmail.com>
2017-05-07 12:57:35 -0700

    Checkpoint

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from simulators.stock_market.trader import Trader
 
 
 if __name__ == "__main__":
-    trader = Trader()
-
-    for _ in range(10):
+    for _ in range(10000):
+        trader = Trader()
         brain = Brain(trader.actions, trader.data)
         brain.run_simulation()
         final_value = trader.get_current_value(trader.data[-1]['price'])
